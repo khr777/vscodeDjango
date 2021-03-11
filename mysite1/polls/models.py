60337ddb15183921660daacca9358ba4ae4773dc
khr777 <kim5638yw@gmail.com>
2021-03-11 15:38:58 +0900

    datetime import하고, 관계형 데이터 추가 및 수정, 추가, 삭제 연습
    datetime import하고, 관계형 데이터 추가 및 수정, 추가, 삭제 연습.

--- mysite1/polls/models.py
+++ mysite1/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_txt = models.CharField(max_length=200)
@@ -7,6 +8,12 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_txt
+    
+    # 가장 최근 데이터가 있는지 없는지를 판단하기 위한 메서드
+    def published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+		# 하루를 뺀 값인 24시간 안에 있는 데이터가 있느냐 없느냐 확인하기 위한 방법.
+		# 최신 데이터인지를 판단하는.
     
 
 class Choice(models.Model):
